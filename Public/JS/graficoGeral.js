2670b17f1989bec304053b12449a7105bdbccf4e
PaolaVeloso0907 <paola.paula@sptech.school>
2026-06-03 22:53:37 -0300

    Deixando as KPI's dinâmicas do painel geral

--- Public/JS/graficoGeral.js
+++ Public/JS/graficoGeral.js
@@ -1,73 +1,17 @@
-function carregarDadosSensor() {
-  let idUsuario = sessionStorage.getItem("ID_USUARIO");
+var graficoBarra;
 
-  fetch("/dashboard/buscar-dados-sensor", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      idUsuario: idUsuario,
-    }),
-  })
-    .then(function (resposta) {
-      if (resposta.ok) {
-        console.log(resposta);
-
-        resposta.json().then((json) => {
-          console.log(json);
-
-          gerarGrafico(json);
-        });
-      } else {
-        alert("Erro ao buscar dados do sensor!");
-      }
-    })
-    .catch(function (erro) {
-      alert("Erro ao buscar dados do sensor!");
-    });
-}
-
-function atualizarDadosGrafico() {
-  console.log("Atualizei os dados!");
-  carregarDadosSensor();
-  setInterval(carregarDadosSensor, 7000);
-}
-
-function gerarGrafico(data) {
-  let labelsBarra = [];
-  let dadosBarra = [];
-
-  for (let i = 0; i < data.length; i++) {
-    labelsBarra.push(data[i].codigoTanque);
-    dadosBarra.push(Number(data[i].temperatura).toFixed(0));
-  }
-
-  let cores = [];
-
-  for (let i = 0; i < dadosBarra.length; i++) {
-    let v = dadosBarra[i];
-
-    if (v < 20) {
-      cores.push("#E6982B");
-    } else if (v > 30) {
-      cores.push("#C52A2A");
-    } else {
-      cores.push("#6BBF61");
-    }
-  }
-
-  new Chart(document.getElementById("graficoBarra"), {
+function iniciarGrafico() {
+  graficoBarra = new Chart(document.getElementById("graficoBarra"), {
     type: "bar",
 
     data: {
-      labels: labelsBarra,
+      labels: [],
 
       datasets: [
         {
           label: "Temperatura",
-          data: dadosBarra,
-          backgroundColor: cores,
+          data: [],
+          backgroundColor: [],
           borderRadius: 8,
         },
       ],
@@ -110,6 +54,77 @@ function gerarGrafico(data) {
   });
 }
 
+function atualizarDadosGrafico() {
+  console.log("Inicializando o gráfico e buscando dados...");
+
+  iniciarGrafico();
+  carregarDadosSensor();
+
+  setInterval(carregarDadosSensor, 10000);
+}
+
+function carregarDadosSensor() {
+  let idUsuario = sessionStorage.getItem("ID_USUARIO");
+
+  fetch("/dashboard/buscar-dados-sensor", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      idUsuario: idUsuario,
+    }),
+  })
+    .then(function (resposta) {
+      if (resposta.ok) {
+        console.log(resposta);
+
+        resposta.json().then((json) => {
+          console.log(json);
+          gerarKPI(json);
+          gerarGrafico(json, graficoBarra);
+        });
+      } else {
+        alert("Erro ao buscar dados do sensor!");
+      }
+    })
+    .catch(function (erro) {
+      alert("Erro ao buscar dados do sensor!");
+    });
+}
+
+function gerarGrafico(data, grafico) {
+  let labelsBarra = [];
+  let dadosBarra = [];
+
+  console.log(grafico.data);
+
+  for (let i = 0; i < data.length; i++) {
+    labelsBarra.push(data[i].codigoTanque);
+    dadosBarra.push(Number(data[i].temperatura).toFixed(0));
+  }
+
+  cores = [];
+
+  for (let i = 0; i < dadosBarra.length; i++) {
+    let temperatura = dadosBarra[i];
+
+    if (temperatura < 20) {
+      cores.push("#E6982B");
+    } else if (temperatura > 30) {
+      cores.push("#C52A2A");
+    } else {
+      cores.push("#6BBF61");
+    }
+  }
+
+  grafico.data.labels = labelsBarra;
+  grafico.data.datasets[0].data = dadosBarra;
+  grafico.data.datasets[0].backgroundColor = cores;
+
+  grafico.update();
+}
+
 function mudarParaEspecifico() {
   let select = document.getElementById("select_especifico");
   let tanqueSelecionado = select.value;
@@ -127,6 +142,30 @@ function mudarParaEspecifico() {
   }
 
   console.log("O usuário escolheu o tanque: " + tanqueEscolhido);
+}
 
+// funcao KPI's
+function gerarKPI(data) {
+  let totalDeTanques = 0;
+  let totalDeSensoresInativos = 0;
+  let totalAbaixoDoIdeal = 0;
+  let totalAcimaDoIdeal = 0;
 
+  totalDeTanques = data.length;
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].statusSensor == 0) {
+      totalDeSensoresInativos++;
+    }
+
+    if (data[i].temperatura <= 20) {
+      totalAbaixoDoIdeal++;
+    } else if (data[i].temperatura >= 30) {
+      totalAcimaDoIdeal++;
+    }
+  }
+  document.getElementById("sensoresInativos").innerText = totalDeSensoresInativos;
+  document.getElementById("totalAbaixoIdeal").innerText = totalAbaixoDoIdeal;
+  document.getElementById("totalAcimaIdeal").innerText = totalAcimaDoIdeal;
+  document.getElementById("TotalTanques").innerText = totalDeTanques;
 }
--- src/models/registroModel.js
+++ src/models/registroModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function buscarDadosSensor(idUsuario) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", idUsuario)
     var instrucaoSql = `
-        SELECT r.*, t.codigoTanque
+        SELECT r.*, t.codigoTanque, s.statusSensor
         FROM registro r 
         JOIN sensor s ON s.idSensor = r.fkSensor 
         JOIN tanque t ON t.idTanque = s.fkTanque
