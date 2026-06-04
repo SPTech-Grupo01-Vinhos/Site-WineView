a413cd129c94671218d9c2ca86aac08a6023b293
PaolaVeloso0907 <paola.paula@sptech.school>
2026-06-04 16:43:44 -0300

    Feito pequenos últimos ajustes

--- Public/JS/simulador.js
+++ Public/JS/simulador.js
@@ -1,7 +1,7 @@
 function simular_vinho() {
         let producao_litros_mes = Number(input_producao_mensal.value) // 87000
         let custo_litro = Number(input_custo_litro.value) // 15
-        let valor_venda_garrafa = Number(input_valor_garrafa.value) // 60
+        let valor_venda_garrafa = Number(input_valor_garrafa.value) // 30
 
         // Quantidade de garrafas cheias, onde 750 ml = 0,75 litros
         let total_garrafas_mes = producao_litros_mes / 0.75  
@@ -13,28 +13,27 @@ function simular_vinho() {
 
         // Quantidade que ele ganha vendendo todas as garrafas cheias 
         let total_bruto_mes = total_garrafas_mes * valor_venda_garrafa
-        //                        (87000/0.75)   *   60
+        //                        (87000/0.75)   *   30
         
         // Quanto ele ganha tirando os custos de produção (total do vinho dentro)
         let lucro_estimado_mes = total_bruto_mes - custo_total_litro_mes
-        //                     ((87000/0.75)   *   60) - (15    *  87000)
+        //                     ((87000/0.75)   *   30) - (15    *  87000)
 
         //====================================================================================
         
         // Supondo que ao fermentar o vinho, perde 10% 
         let perda_10_porcento = total_bruto_mes * 0.10 
-        //               ((87000/0.75)   *   60)  *  0.10
+        //               ((87000/0.75)   *   30)  *  0.10
         let perda_10_anual = perda_10_porcento * 12
-        //               (((87000/0.75)   *   60)  *  0.10)  * 12  
+        //               (((87000/0.75)   *   30)  *  0.10)  * 12  
 
         // Supondo que usando o monitoramento ele só perde 2%
         let perda_2_porcento = total_bruto_mes * 0.02
-        //               ((87000/0.75)   *   60) * 0.02
+        //               ((87000/0.75)   *   30) * 0.02
 
         let economia_mensal = perda_10_porcento - perda_2_porcento
-        //                (((87000/0.75)   *   60)  *  0.10) - (((87000/0.75)   *   60) * 0.02)
+        //                (((87000/0.75)   *   30)  *  0.10) - (((87000/0.75)   *   30) * 0.02)
         let economia_anual = economia_mensal * 12
-        // 
 
         div_mensagem.innerHTML = `
         <div class="card-resultado">
